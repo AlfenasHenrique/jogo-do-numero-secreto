2d264d1c73d4ccf88f397ffc1f5ff8e7ea14dc2c
AlfenasHenrique <henriquealfenas22@gmail.com>
2024-11-18 10:07:12 -0300

    Delete js-curso-2-aula_-/Testespontuais.js

--- js-curso-2-aula_-/Testespontuais.js
+++
@@ -1,6 +0,0 @@
-var frutas = ["maçã", "pera"]
-console.log(frutas.length)
-
-let listaGenerica = []
-let linguagens = [ 'JavaScript','C','C++', 'Kotlin','Python']
-console.log(linguagens.length('JavaScript'))
