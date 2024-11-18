8845252ae2c92647edbfe0fc1a7b9e2d0f7b0dff
AlfenasHenrique <henriquealfenas22@gmail.com>
2024-11-18 10:01:57 -0300

    Add files via upload

---
+++ js-curso-2-aula_-/Testespontuais.js
@@ -0,0 +1,6 @@
+var frutas = ["maçã", "pera"]
+console.log(frutas.length)
+
+let listaGenerica = []
+let linguagens = [ 'JavaScript','C','C++', 'Kotlin','Python']
+console.log(linguagens.length('JavaScript'))
